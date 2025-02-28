eb76da3bec88e15fa322cb98bd854c89cb00bb13
shahidulllah <mdshahidsumon177@gmail.com>
2025-02-28 22:42:42 +0600

    modify

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            <h1>This is about page</h1>
+            <h1>This is About Page</h1>
         </div>
     );
 };
